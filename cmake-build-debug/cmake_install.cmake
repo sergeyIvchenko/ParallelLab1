cd3e11d6c7ddbe53c38ba523305a183c21575cd1
mrkakawiteo <loveyourswain@gmail.com>
2023-03-08 05:52:31 +0200

    Finished multi-thread function with a refactoring. Added choosing the amount of threads.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "D:/CLion 2022.1.2/bin/mingw/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/study/parralel/lab1/cmake-build-debug/googletest/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
